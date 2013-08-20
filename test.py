32b3c314a35bac0f7bb20563143e5e18f06ee363
EC2 Default User <ec2-user@ip-172-31-8-143.ap-southeast-1.compute.internal>
2013-08-20 08:54:10 +0000

    init project

---
+++ test.py
@@ -0,0 +1,19 @@
+import os
+import re
+os.system("speedtest-cli --list > server.txt")
+
+re_line = re.compile(r"([\d]+)\) (.*) \((.*), (.*)\) \[(.*) km\]")
+vs = []
+with open("server.txt") as reader:
+	for line in reader:
+		p= re_line.findall(line)
+		if p:
+			vs.append(p[0])
+
+vs.sort(key=lambda i: i[0])
+for v in vs:
+	print v
+	server_id, name, location, counter, distance = v
+	if counter == "Taiwan":
+		print "start testing", v
+		os.system("speedtest-cli --server %s > %s.log"%(server_id, server_id))	
